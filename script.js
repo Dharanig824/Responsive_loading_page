7b91e7a90938df0864494bf5fd8f2e7eaa5aa10d
Dharanig824 <dharanig26042005@gmail.com>
2026-06-02 11:37:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Select Navbar
+const navbar = document.getElementById("navbar");
+
+// Change navbar style on scroll
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    }
+    else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
